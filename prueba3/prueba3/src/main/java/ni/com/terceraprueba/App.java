9faac4f80aff24bf1c20d4829f88ee70abb9688f
Martha Suarez <titasp2013@gmail.com>
2017-11-11 14:26:59 -0600

    Proyectos Completos Proyectos 2,3,4 y 5

---
+++ prueba3/prueba3/src/main/java/ni/com/terceraprueba/App.java
@@ -0,0 +1,13 @@
+package ni.com.terceraprueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ prueba5/prueba5/src/main/java/ni/com/quintaprueba/App.java
@@ -0,0 +1,13 @@
+package ni.com.quintaprueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
