570cc0e512c68bcde7c9ca75d73ee3687e1bdc0a
Martha Suarez <titasp2013@gmail.com>
2017-11-11 14:14:42 -0600

    Practica 1 Maven Primer proyecto

---
+++ prueba1/prueba1/src/main/java/ni/com/primeraprueba/App.java
@@ -0,0 +1,13 @@
+package ni.com.primeraprueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
